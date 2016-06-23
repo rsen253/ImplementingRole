4631a21b2990f8e2f6b1266082d16e069bb340b2
rsen253 <rsen253@gmail.com>
2016-06-23 18:02:24 +0530

    Added data to the database

---
+++ ImplementRole/App_Start/IdentityConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImplementRole
+{
+    public class IdentityConfig
+    {
+
+    }
+}
--- ImplementRole/Controllers/HomeController.cs
+++ ImplementRole/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,8 +12,25 @@ namespace ImplementRole.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            var context = new IdentityDbContext<IdentityUser>();
+            var userStore = new UserStore<IdentityUser>(context);
+            var manager = new UserManager<IdentityUser>(userStore);
+
+            var email = "foo@bar.com";
+            var password = "Passw0rd";
+            var user = await manager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                user = new IdentityUser
+                {
+                    UserName = email,
+                    Email = email
+                };
+                await manager.CreateAsync(user, password);
+            }
             return Content("Hello Index");
         }
     }
