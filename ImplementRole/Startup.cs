3859b851aa8ba231ab004fcbccc3db49171db0e4
rsen253 <rsen253@gmail.com>
2016-06-23 17:49:29 +0530

    Added nuget packages in a empty project and configure the identity

---
+++ ImplementRole/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImplementRole.Startup))]
+namespace ImplementRole
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        { }
+    }
+}
